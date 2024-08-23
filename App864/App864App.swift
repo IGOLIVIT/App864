86d2f4ae5ce9e6e6669a6b4d5ab573bb5ca834fe
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-08-23 16:07:42 +0300

    Initial Commit

---
+++ App864/App864App.swift
@@ -0,0 +1,17 @@
+//
+//  App864App.swift
+//  App864
+//
+//  Created by IGOR on 23/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct App864App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
